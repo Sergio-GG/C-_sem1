53bf4b1623cb0eeaa0c3000ea80dde76c1b82029
Sergio <sergio-guitar@yandex.ru>
2023-02-14 11:34:15 +0300

    Second task

---
+++ TASK02_largerNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB) 
+{
+    Console.WriteLine($"max = {numberA}, min = {numberB}");
+}
+if (numberA < numberB) 
+{
+    Console.WriteLine($"max = {numberB}, min = {numberA}");
+}
+if (numberA == numberB) 
+{
+    Console.WriteLine("Числа равны");
+}
