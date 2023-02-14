8acd0847679c84122133c82f31a7b1c40de77d87
Sergio <sergio-guitar@yandex.ru>
2023-02-14 12:00:39 +0300

    Sixth task

---
+++ TASK06_EvenOrOdd/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int numberX = Convert.ToInt32(Console.ReadLine());
+
+if (numberX % 2 == 0) 
+{
+    Console.WriteLine("Число является четным");
+}
+else Console.WriteLine("Число не является четным");
