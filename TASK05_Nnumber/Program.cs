bf196ff01365bb391d2b79118f7839d1ab3b4510
Sergio <sergio-guitar@yandex.ru>
2023-02-14 10:14:15 +0300

    Fifth task

---
+++ TASK05_Nnumber/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = -number;
+while (counter <= number)
+{
+    Console.Write(counter + " ");
+    counter++;
+}
