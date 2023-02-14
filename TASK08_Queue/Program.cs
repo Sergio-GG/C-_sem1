78b79403f60042e81259e762649cd5448e88403b
Sergio <sergio-guitar@yandex.ru>
2023-02-14 12:17:24 +0300

    Eighth task

---
+++ TASK08_Queue/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 1)
+{
+    int counter = 1;
+    while (counter <= number)
+    {
+        if (counter % 2 == 0)
+        {
+            Console.Write(counter + " ");
+        }
+        counter++;
+    }
+}
+else Console.WriteLine("Введите число больше единицы");
