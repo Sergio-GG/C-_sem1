1b29ddabfb918c9a59ec28beec96d5b99a1ecd9c
Sergio <sergio-guitar@yandex.ru>
2023-02-14 11:12:48 +0300

    Seventh task

---
+++ TASK07_lastNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите техзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int result = number % 10;
+    Console.WriteLine("Последняя цифра числа --> " + result);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
