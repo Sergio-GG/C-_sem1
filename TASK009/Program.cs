dfe32fb24f4e1f21c409a5b4c50276494683cb3c
Sergio <sergio-guitar@yandex.ru>
2023-02-16 19:48:52 +0300

    ninth task

---
+++ TASK009/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая
+// 1. генерирует случайное число от 10 до 99
+// 2. выводит на экран большую цифру из этого числа
+
+int number = Convert.ToInt32(new Random().Next(10, 100));
+Console.WriteLine(number);
+int maxnum = MaxNumber(number);
+Console.WriteLine($"Наибольшее число --> {maxnum}");
+
+// Method
+int MaxNumber(int num)
+{
+    int firstNumber = num / 10;
+    int secondNumber = number % 10;
+    int res = firstNumber > secondNumber ? firstNumber : secondNumber;
+    return res;
+}
