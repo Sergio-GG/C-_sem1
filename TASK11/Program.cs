e81955a037d7e942f38dc38edf955654d05cd7c4
Sergio <sergio-guitar@yandex.ru>
2023-02-16 20:30:09 +0300

    eleventh task

---
+++ TASK11/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа
+int number = Convert.ToInt32(new Random().Next(100, 1000));
+Console.WriteLine(number);
+int newNumber = Strip(number);
+Console.WriteLine($"Новое число --> {newNumber}");
+
+// Method
+
+int Strip (int num)
+{
+    int firstNum = num / 100;
+    int secondNum = num % 10;
+    return firstNum * 10 + secondNum;
+}
+
+
