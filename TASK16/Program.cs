a7d059a67d98e3c54cc5cbbfd84920f4dd11cd86
Sergio <sergio-guitar@yandex.ru>
2023-02-17 12:45:45 +0300

    Sixteenth task

---
+++ TASK16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принимает два числа
+// и проверяет, является ли ли одно из них квадратом другого
+
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (Square(numberA,numberB)) Console. WriteLine ("Да");
+else Console. WriteLine ("Нет");
+
+
+
+// Method
+
+bool Square(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
