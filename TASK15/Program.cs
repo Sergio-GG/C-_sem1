6af71ac8c055896079556ef31f927a8d1a8a6919
Sergio <sergio-guitar@yandex.ru>
2023-02-17 13:50:11 +0300

    Fifteenth task

---
+++ TASK15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа принимает число, соответсвующее дню недели
+// и и определяет, является ли день выходным.
+
+
+
+Console. WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA > 0 && numberA < 8)
+{
+    if (Day(numberA)) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
+else Console.WriteLine("Введите число от 1 до 7 включительно");
+
+
+
+// Method
+bool Day(int num)
+{
+    return num > 5;
+}
