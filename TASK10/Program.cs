7bad9d36a84307ae29fd99a06ca99110863da22b
Sergio <sergio-guitar@yandex.ru>
2023-02-17 12:56:36 +0300

    Tenth task

---
+++ TASK10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа выводит вторую цифру 
+// трехзначного числа
+
+
+Console.WriteLine("Введите техзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int res = ThreeNum (number);
+    Console.WriteLine("Вторая цифра числа --> " + res);
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число");
+}
+
+// Method
+
+int ThreeNum(int num)
+{
+    int firstStep = number / 10;
+    int secondStep = firstStep % 10;
+    return secondStep;
+}
