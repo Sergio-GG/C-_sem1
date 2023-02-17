76fb1e928bdd8e195800de8ba0aea73249ad74a9
Sergio <sergio-guitar@yandex.ru>
2023-02-17 12:15:17 +0300

    Twelfth task

---
+++ TASK12/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB == 0) Console.WriteLine("Введите любое число кроме нуля");
+
+int last = numberA % numberB;
+int newNumber = Multyplicity(numberA, numberB);
+if (newNumber == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"Не кратно. Остаток от деления -> {last}");
+
+
+// Method
+
+int Multyplicity(int num1, int num2)
+{
+    int res = num1 % num2;
+    return res;
+}
