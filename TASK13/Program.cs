34068b1618e743b688dc236dc3d21a0510f1ed55
Sergio <sergio-guitar@yandex.ru>
2023-02-17 13:38:12 +0300

    Thirteenth task

---
+++ TASK13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа выводит третью цифру числа,
+// если таковая имеется.
+
+
+Console. WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA > 99)
+{
+    int res = Strip(numberA);
+    Console.WriteLine(res);
+}
+else Console.WriteLine("Тертьей цифры нет");
+
+
+// Method
+
+int Strip (int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    } 
+    int number = num % 10;
+    return number;
+}
