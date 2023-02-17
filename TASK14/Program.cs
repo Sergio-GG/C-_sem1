daaef9175e7e47bec787008ac622e4f25e7d274e
Sergio <sergio-guitar@yandex.ru>
2023-02-17 12:33:15 +0300

    Forteenth task

---
+++ TASK14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Является ли число кратным 7 и 23
+
+
+Console.WriteLine("Введите число");
+int numberX = Convert.ToInt32(Console.ReadLine());
+
+
+if (Ismulty(numberX)) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
+// Method
+
+bool Ismulty (int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
