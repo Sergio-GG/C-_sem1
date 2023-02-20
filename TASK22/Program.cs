44dccbb0ea069dc3868f42fb369a5994b8980864
Sergio <sergio-guitar@yandex.ru>
2023-02-20 21:20:18 +0300

    22 task

---
+++ TASK22/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принимает на вход число N
+// и выдает таблицу квадартов числел
+// от 1 до N
+
+
+Console.WriteLine("Введите число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+ProgN(numberN);
+
+
+// Method
+
+void ProgN (int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i,5}    {Math.Pow(i, 2),5}");
+    }
+}
