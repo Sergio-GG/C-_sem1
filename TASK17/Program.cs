640c587d0c1418ec11c4d7884d047a2820990916
Sergio <sergio-guitar@yandex.ru>
2023-02-20 17:10:59 +0300

    17 task

---
+++ TASK17/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите координаты");
+Console.Write("X: ");
+int xC = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yC = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xC, yC);
+string result = quarter > 0
+                ? $"Указанные координаты соответствуют {quarter} четверти"
+                : "Введены некооректные координаты";
+Console.WriteLine(result);
+
+//Method 
+
+int Quarter(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    else return 0;
+}
