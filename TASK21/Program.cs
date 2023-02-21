68d85933bad8e0b970eae28e968b4d349a726af2
Sergio <sergio-guitar@yandex.ru>
2023-02-21 12:56:16 +0300

    21 task

---
+++ TASK21/Program.cs
@@ -0,0 +1,34 @@
+﻿// Программа находит расстояние между двумя точками
+// в 3D пространстве.
+
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("X: ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int Az = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X: ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int By = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int Bz = Convert.ToInt32(Console.ReadLine());
+
+double result = Distance(Ax, Ay, Az, Bx, By, Bz);
+Console.WriteLine($"Расстояние между точками равно {result}");
+
+
+// Method 
+
+double Distance ( int x1,int y1, int z1, int x2, int y2, int z2)
+{
+    int Xdiff = x2 - x1;
+    int Ydiff = y2 - y1;
+    int Zdiff = z2 - z1;
+    double dist = Math.Sqrt(Math.Pow(Xdiff, 2) + Math.Pow(Ydiff, 2) + Math.Pow(Zdiff, 2));
+    double distRound = Math.Round(dist, 2, MidpointRounding.ToZero);
+    return distRound;
+}
