4f2c59e92c06c4d23453f8c6e2bc4ce2d15384c2
Sergio <sergio-guitar@yandex.ru>
2023-02-21 20:58:52 +0300

    26 task

---
+++ TASK26/Program.cs
@@ -0,0 +1,20 @@
+﻿// программа принимает число на вход
+// и выводид количество цифр в нем
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = Counter (number);
+Console.WriteLine($"Количество цифр в числе {result}");
+// Method
+
+int Counter (int num)
+{
+    int counter = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        counter++;
+    }
+    return counter;
+}
+
