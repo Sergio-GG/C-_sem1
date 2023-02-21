2cc74baf047cc8d3063588a1abcc65cd9a89c327
Sergio <sergio-guitar@yandex.ru>
2023-02-21 13:18:13 +0300

    23 task

---
+++ TASK23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+CubeNumber(numberN);
+
+// Method
+
+void CubeNumber(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        double cubes = Math.Pow(index, 3);
+        Console.WriteLine($"{index, 5}     {cubes,5}");
+        index++;
+    }
+}
