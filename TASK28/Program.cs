6855e462edf4e024aafed04335830da4f2ee7ada
Sergio <sergio-guitar@yandex.ru>
2023-02-21 21:17:16 +0300

    28 task

---
+++ TASK28/Program.cs
@@ -0,0 +1,20 @@
+﻿// программа возващает произведение всех чисел от 1 до N
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = Counter (number);
+Console.WriteLine($"Произведение равно {result}");
+// Method
+
+int Counter (int num)
+{
+    int multiply = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        multiply = multiply * i;
+    }
+    return multiply;
+}
+
+
