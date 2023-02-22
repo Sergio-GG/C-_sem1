ead1c0ab83bbe18fff2a2f097593627b9d12ed46
Sergio <sergio-guitar@yandex.ru>
2023-02-22 11:09:39 +0300

    27 task edited

---
+++ TASK27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа принимает число 
+// и выдает сумму цифр в числе
+
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int res = Summa(numberA);
+Console.WriteLine($"сумма цифр в числе {numberA} равна: {res}");
+
+// Method
+
+int Summa(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int numA = num % 10;
+        sum = sum + numA;
+        num = num / 10;
+    }
+    return sum;
+}
