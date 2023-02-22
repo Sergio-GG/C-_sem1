a010a53a1831971f364e7fe710549db1cc523c29
Sergio <sergio-guitar@yandex.ru>
2023-02-22 10:27:02 +0300

    25 task

---
+++ TASK25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа принимает на вход два числа A и B
+// и возводит число A в натуральную степень B
+
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB > 0)
+{
+    int result = Exponent(numberA, numberB);
+    Console.WriteLine($"{numberA} в степени {numberB} равно : {result}");
+}
+else Console.WriteLine($"Введите натуральное число B");
+
+// Method
+
+int Exponent(int numA, int numB)
+{   
+    int raiseToPow = 1;
+    for (int i = 0; i < numB; i++)
+    {
+        checked
+        {
+            raiseToPow = raiseToPow * numA;
+        }
+    }
+    return raiseToPow;
+}
