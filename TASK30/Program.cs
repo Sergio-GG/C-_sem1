8566473902b0774896c7a876d8b531880f44f008
Sergio <sergio-guitar@yandex.ru>
2023-02-22 09:57:03 +0300

    30 task

---
+++ TASK30/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+//Method
+
+int[] FillArray( int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+    return arr;
+}
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    
+    }
+}
