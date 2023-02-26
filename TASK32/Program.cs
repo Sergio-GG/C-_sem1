fc54eb9210ac3463b34c4d3d285e5e2a55fd500b
Sergio <sergio-guitar@yandex.ru>
2023-02-26 13:30:13 +0300

    32 task

---
+++ TASK32/Program.cs
@@ -0,0 +1,43 @@
+﻿// замена элементов массива на противоположные
+
+// Method 1 
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int [] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+        
+    }    
+    Console.Write("]");
+    
+}
+
+int[] Invertion (int[] arr)
+{
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+    }
+    return arr;
+}
+
+
+int[] array = CreateArrayRndInt(12,-9,9);
+PrintArray(array);
+Console.WriteLine();
+Invertion(array);
+PrintArray(array);
