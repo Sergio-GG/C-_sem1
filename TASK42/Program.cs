8161436c9e2e9f07a7befcc5f3548749aada01de
Sergio <sergio-guitar@yandex.ru>
2023-03-01 12:03:54 +0300

    42 task

---
+++ TASK42/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DecToBin (int num)
+{
+    int count = 1;
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 2 * count;
+        num = num / 2;
+        count *= 10;
+    }
+    return result;
+}
+int decToBin = DecToBin(number);
+System.Console.WriteLine($"{number} --> {decToBin}");
