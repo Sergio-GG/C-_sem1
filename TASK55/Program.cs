5eddc63f0dfffc9cc9cdb238cac1f8f496119804
Sergio <sergio-guitar@yandex.ru>
2023-03-09 10:47:05 +0300

    55  edit

---
+++ TASK55/Program.cs
@@ -0,0 +1,59 @@
+﻿
+int[,] CreateMatrixRndInt(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+
+    }
+    return matrix;
+}
+
+
+int[,] ReplaceToColumns(int[,] matrix)
+{
+    int[,] matr1 = new int[matrix.GetLength(0), matrix.GetLength(1)];
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matr1[i, j] = matrix[j, i];
+        }
+    }
+
+    return matr1;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.Write($"{matrix[i, j],3}");
+
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array = CreateMatrixRndInt(4, 4);
+PrintMatrix(array);
+System.Console.WriteLine();
+int[,] newarr = ReplaceToColumns(array);
+PrintMatrix(newarr);
+
+
+
+
+
+
