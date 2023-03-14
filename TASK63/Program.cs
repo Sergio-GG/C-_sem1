88225334298748834b963693c17dc1c753e09a58
Sergio <sergio-guitar@yandex.ru>
2023-03-14 16:40:02 +0300

    63 task

---
+++ TASK63/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа принимает на ввод натуральное число N 
+//  и выводит на экран все натуральные числа от  1 до N
+
+Console.WriteLine("Введите натуральное число: ");
+int number = int.Parse(Console.ReadLine());
+
+NaturalNumbers(number);
+
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
