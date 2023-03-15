d9116571f434e59cc2c8f2148d576f137f259719
Sergio <sergio-guitar@yandex.ru>
2023-03-15 10:47:41 +0300

    64 task

---
+++ TASK64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте занчение N. напишиет программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+
+
+Console.WriteLine("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine());
+PrintNumbers(numberN);
+
+void PrintNumbers(int num)
+{
+    if (num == 0 ) return;
+    else if (num > 1) Console.Write($"{num}, ");
+    else System.Console.WriteLine(num);
+    PrintNumbers(num - 1);
+}
